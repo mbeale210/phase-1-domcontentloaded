4b64558f43bd3e95265b68464d39177a68ffd0ce
Mark Beale <bealebeaverst@gmail.com>
2024-07-17 03:35:55 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  document.querySelector("#text").innerHTML = "This is really cool!";
+});
 // Your code goes here
+console.log("This is before the Dom is loaded")
